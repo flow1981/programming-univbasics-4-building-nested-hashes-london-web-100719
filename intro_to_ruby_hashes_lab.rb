c4c977d034174cffdc917e314816fe7cd58b0dc8
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-30 15:08:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
                                          four_pieces_owned:200},
                     :names => { :reading_railroad => {},
                                 :pennsylvania_railroad => {},
-                                :b_ando_railroad => {},
+                                :b_and_o_railroad => {},
                                 name4:"four"}
       
     }
